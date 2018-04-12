f4146e8e6c1b2d2dd68d5c940c21167b5e051b41
Jay Palan <jpalan.gmu@gmail.com>
2018-04-12 18:18:21 -0400

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HousesComponent } from './houses/houses.component';
+import { HouseListComponent } from './houses/house-list/house-list.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HousesComponent,
+    HouseListComponent,
+    HouseDetailComponent,
   ],
   imports: [
     BrowserModule
