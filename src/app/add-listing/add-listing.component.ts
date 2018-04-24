99c86b89fcd18abc90e4522d0fdb9d1489cfe92b
Jay Palan <jpalan.gmu@gmail.com>
2018-04-24 00:59:31 -0400

    added 2nd question to listing

--- src/app/add-listing/add-listing.component.ts
+++ src/app/add-listing/add-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { House } from '../house.mode';
 
 @Component({
   selector: 'app-add-listing',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-listing.component.css']
 })
 export class AddListingComponent implements OnInit {
+  newHouse: House;
+  firstQuestion: boolean;
+  secondQuestion: boolean;
+  thirdQuestion: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.firstQuestion = true;
+    this.secondQuestion = false;
+    this.thirdQuestion = false;
+  }
+
+  nextQuestion(questionNo: string) {
+    if (questionNo === 'second') {
+      this.firstQuestion = false;
+      this.secondQuestion = true;
+    }
+  }
+
+  previousQuestion(questionNo: string) {
+    if (questionNo === 'first') {
+      this.firstQuestion = true;
+      this.secondQuestion = false;
+    }
+    console.log(questionNo);
+    console.log(this.firstQuestion);
+    console.log(this.secondQuestion);
+  }
+
+  setMyStyles() {
+    const styles = {
+      display: this.secondQuestion ? 'block' : 'none'
+    };
+    return styles;
+  }
+
+  setFirstStyles() {
+    const styles = {
+      display: this.firstQuestion ? 'block' : 'none'
+    };
+    return styles;
   }
 
 }
